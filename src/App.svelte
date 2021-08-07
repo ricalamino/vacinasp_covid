8c6464c4c337ff0af81194966c170881f61388fe
Ricardo Alamino <ricalamino@gmail.com>
2021-08-06 22:27:18 -0300

    24 a 21 anos atualizado na cidade de Sampa

--- src/App.svelte
+++ src/App.svelte
@@ -88,12 +88,12 @@
 		{ id:34, idade: 26, ano: 2021, mes:8, dia:5},
 		{ id:35, idade: 25, ano: 2021, mes:8, dia:6},
 		{ id:36, idade: 24, ano: 2021, mes:8, dia:10},
-		{ id:37, idade: 23, ano: 2021, mes:8, dia:10},
-		{ id:38, idade: 22, ano: 2021, mes:8, dia:10},
-		{ id:39, idade: 21, ano: 2021, mes:8, dia:10},
-		{ id:40, idade: 20, ano: 2021, mes:8, dia:10},
-		{ id:41, idade: 19, ano: 2021, mes:8, dia:10},
-		{ id:42, idade: 18, ano: 2021, mes:8, dia:10},
+		{ id:37, idade: 23, ano: 2021, mes:8, dia:12},
+		{ id:38, idade: 22, ano: 2021, mes:8, dia:13},
+		{ id:39, idade: 21, ano: 2021, mes:8, dia:14},
+		{ id:40, idade: 20, ano: 2021, mes:8, dia:20},
+		{ id:41, idade: 19, ano: 2021, mes:8, dia:20},
+		{ id:42, idade: 18, ano: 2021, mes:8, dia:20},
 		{ id:43, idade: 17, ano: 2021, mes:8, dia:30},
 		{ id:44, idade: 16, ano: 2021, mes:8, dia:30},
 		{ id:45, idade: 15, ano: 2021, mes:8, dia:30},
@@ -104,7 +104,7 @@
 
 
 	// Datas da última atualização de cada calendário
-	const ultima_data_cidade = '03/08/2021';
+	const ultima_data_cidade = '06/08/2021';
 	const ultima_data_estado = '29/07/2021';
 	
 	let selected = 1;
